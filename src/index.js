643b78d4a1db30b23e788c83238955d99112e950
iceman121212 <9387729+iceman121212@users.noreply.github.com>
2021-03-27 16:03:53 -0600

    walk through up through using react-redux package

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import noteReducer from './reducers/noteReducer'
 
+const generateId = () => Math.floor(Math.random() * 1000000)
 
 const store = createStore(noteReducer)
 
@@ -25,22 +26,67 @@ store.dispatch({
 })
 
 
+const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    data: {
+      content, important: false, id: generateId()
+    }
+  }
+}
+
+const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    data: { id: id }
+  }
+}
+
+const addNote = (event) => {
+  event.preventDefault()
+  const content = event.target.note.value
+
+  console.log(event)
+  console.log('\n^^^ event')
+  console.log(event.target)
+  console.log('\n^^^ target')
+  console.log(event.target.note)
+  console.log('\n^^^ event.target.note')
+  console.log(event.target.note.value)
+  console.log('\n^^^ event.target.note.value')
+
+  store.dispatch(createNote(content))
+}
+
+const toggleImportance = (id) => {
+  store.dispatch(toggleImportanceOf(id))
+}
+
+
 const App = () => {
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name='note' />
+        <button type='submit'>add</button>
+      </form>
       <ul>
         {store.getState().map(note =>
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          <li
+            key={note.id}
+            onClick={() => toggleImportance(note.id)}
+          >
+            {note.content}
+            <strong>{note.important ? 'important' : ''}</strong>
           </li>)}
       </ul>
-    </div>
+    </div >
   )
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp)
