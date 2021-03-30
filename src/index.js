6d199c74c79646794fc979377b1c6dbd1c4846d7
iceman121212 <9387729+iceman121212@users.noreply.github.com>
2021-03-30 08:12:46 -0600

    revert to latest commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, combineReducers } from 'redux'
@@ -16,10 +17,22 @@ const store = createStore(reducer, composeWithDevTools())
 
 console.log(store.getState())
 console.log('\n^^^ store.getState()')
+=======
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+>>>>>>> parent of 9fd251c (walkthrough up to 6.1 complete)
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-)
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
